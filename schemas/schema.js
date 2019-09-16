9186b3e3c5c80b714c336550b7972a61a956a945
Rune Botten <rbotten@gmail.com>
2019-09-16 15:40:26 -0700

    blank schema

--- schemas/schema.js
+++ schemas/schema.js
@@ -12,29 +12,5 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
-
-    // https://www.sanity.io/docs/schema-types/document-type
-    {
-      title: "Post",
-      name: "post",
-      type: "document",
-      fields: [
-        // Add schema types here
-        // https://www.sanity.io/docs/schema-types/string-type
-        {
-          title: "Title",
-          name: "title",
-          type: "string"
-        },
-        // https://www.sanity.io/docs/schema-types/block-type
-        {
-          title: "Body",
-          name: "body",
-          type: "array",
-          of: [{ type: "block" }, { type: "geopoint" }]
-        }
-      ],
-      liveEdit: false
-    }
   ])
 });
