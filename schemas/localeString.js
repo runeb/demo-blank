40d2ab4906c0f95b6c8e5cc4b28a829d3c22560f
Rune Botten <rbotten@gmail.com>
2019-11-07 18:16:26 +0100

    add mux

---
+++ schemas/localeString.js
@@ -0,0 +1,23 @@
+const supportedLanguages = [
+  { id: "en", title: "English", isDefault: true },
+  { id: "es", title: "Spanish" },
+  { id: "nb", title: "Norwegian" }
+];
+
+export default {
+  name: "localeString",
+  type: "object",
+  fieldsets: [
+    {
+      title: "Translations",
+      name: "translations",
+      options: { collapsible: true }
+    }
+  ],
+  fields: supportedLanguages.map(lang => ({
+    title: lang.title,
+    name: lang.id,
+    type: "string",
+    fieldset: lang.isDefault ? null : "translations"
+  }))
+};
